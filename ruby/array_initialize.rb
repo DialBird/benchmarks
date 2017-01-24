8adb5bc97a794c27b281ec5c69378fd634e8e967
keisuke <rememberthatk@icloud.com>
2017-01-24 10:11:49 +0900

    ruby配列の初期化ベンチマーク

---
+++ ruby/array_initialize.rb
@@ -0,0 +1,22 @@
+
+N = 999999999
+
+p "[] * is"
+[false] * N
+
+# p "Array * is"
+# Array.new(N, false)
+
+
+# 2017/01/24の結果
+
+#   "Array * is"
+
+#   real  0m6.086s
+#   user  0m1.758s
+#   sys 0m3.816s
+#   "[] * is"
+#   
+#   real  0m12.255s
+#   user  0m3.384s
+#   sys 0m6.077s
